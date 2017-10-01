769c6d0d0f40ea77455b6152f56ef7a70f861509
zhangzhe <gary.zzhang@gmail.com>
2017-10-01 15:22:21 +0800

    use postrgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170921072846) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "content"
